6443745090dc6e8b6e78331f2c973b3987b62295
BerniPas <bernalpas@gmail.com>
2025-04-05 13:20:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    { // Si ya tienes configuraciones aquí, las mantienes o ajustas },
     host: '0.0.0.0',
     port: 3000,
   },
